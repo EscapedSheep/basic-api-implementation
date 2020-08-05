d7633957eba247604363d8a2e0bffa708446d224
EscapedSheep <sfgmdf001@126.com>
2020-08-05 18:00:21 +0800

    [maida Ge] add log for error

--- src/main/java/com/thoughtworks/rslist/componet/RsEventExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/componet/RsEventExceptionHandler.java
@@ -4,11 +4,13 @@ import com.thoughtworks.rslist.Exception.Error;
 import com.thoughtworks.rslist.Exception.InvalidIndexException;
 import com.thoughtworks.rslist.Exception.InvalidRequestParamException;
 import com.thoughtworks.rslist.api.RsController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice(assignableTypes = {RsController.class})
 public class RsEventExceptionHandler {
 
@@ -23,6 +25,7 @@ public class RsEventExceptionHandler {
 
         Error error = new Error();
         error.setError(errorMessage);
+        log.error(errorMessage);
         return ResponseEntity.badRequest().body(error);
     }
 
--- src/main/java/com/thoughtworks/rslist/componet/UserExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/componet/UserExceptionHandler.java
@@ -2,11 +2,13 @@ package com.thoughtworks.rslist.componet;
 
 import com.thoughtworks.rslist.Exception.Error;
 import com.thoughtworks.rslist.api.UserController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice(assignableTypes = {UserController.class})
 public class UserExceptionHandler {
 
@@ -14,6 +16,7 @@ public class UserExceptionHandler {
     public ResponseEntity handlerInvalidUser() {
         Error error = new Error();
         error.setError("invalid user");
+        log.error("invalid user");
         return ResponseEntity.badRequest().body(error);
     }
 }
